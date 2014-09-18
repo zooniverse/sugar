646cc126ae41181b8ab7a7f1e30b1eb4c62f9205
Michael Parrish <michael@zooniverse.org>
2014-09-18 14:15:44 -0500

    Add notifications

---
+++ migrations/20140902113907_add_unread_index_to_notifications.js
@@ -0,0 +1,11 @@
+'use strict';
+
+exports.up = function(knex, Promise) {
+  return knex.schema.raw('\
+    CREATE INDEX "idx_unread_notifications" ON notifications USING btree(user_id ASC, is_delivered ASC, created_at DESC, expires_at ASC); \
+  ');
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.raw('DROP INDEX idx_unread_notifications;');
+};
