5a883455f7452c9bcbca0ee3a4aa3798e7626de6
Michael Parrish <michael@zooniverse.org>
2014-10-09 11:28:52 -0500

    Refactor notifications to combined user/session key

--- migrations/20141009111158_add_user_key_indexes_to_notifications.js
+++ migrations/20141009111158_add_user_key_indexes_to_notifications.js
@@ -2,7 +2,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.raw('\
-    CREATE INDEX "idx_unread_notifications" ON notifications USING btree(user_id ASC, is_delivered ASC, created_at DESC, expires_at ASC); \
+    CREATE INDEX "idx_unread_notifications" ON notifications USING btree(user_key ASC, is_delivered ASC, created_at DESC, expires_at ASC); \
   ');
 };
 
