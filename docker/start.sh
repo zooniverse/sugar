2bd431906c0a360ae1b8165446f6edafc8c86802
Michael Parrish <michael@zooniverse.org>
2015-05-20 11:45:22 -0500

    Overwrite existing configs on start

--- docker/start.sh
+++ docker/start.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$VAGRANT_APP" ]
 then
-    ln -s /production_config/* /node_app/lib/
+    ln -sf /production_config/* /node_app/lib/
 fi
 
 /node_app/node_modules/.bin/coffee /node_app/index.coffee
