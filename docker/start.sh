af2b567adbaf966c3d4b7d5723daa24de70d9083
Campbell Allen <campbell.allen@gmail.com>
2022-06-07 14:45:38 +0100

    write commit_id.txt file on app start

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Copy commit_id.txt to the public folder or create default
+[ -f ./commit_id.txt ] && cp ./commit_id.txt ./public/commit_id.txt || echo $REVISION > ./public/commit_id.txt
+
 /node_app/node_modules/.bin/coffee /node_app/index.coffee
