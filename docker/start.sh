ac39031351b9694c7c6c6a6225f7a0de668bfd7b
Campbell Allen <campbell.allen@gmail.com>
2023-01-27 02:57:48 +0000

    Revert "modify GC settings (#132)" (#134)
    This reverts commit b8fd1c7d16ca9a13db44626a323f6ee3d01f071f.

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,4 @@
 # Copy commit_id.txt to the public folder or create default
 [ -f ./commit_id.txt ] && cp ./commit_id.txt ./public/commit_id.txt || echo $REVISION > ./public/commit_id.txt
 
-# https://github.com/nodejs/node/blob/main/doc/api/cli.md#--max-semi-space-sizesize-in-megabytes
-# https://github.com/nodejs/node/issues/42511
-node --max-semi-space-size=64 /node_app/index.js
+node /node_app/index.js
