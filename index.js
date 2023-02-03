f4c12e4152fcdf417cd4aa81e0d6b4e0aa29e403
Jim O'Donnell <jim@zooniverse.org>
2023-02-03 14:14:05 +0000

    Add Sentry error logging (#150)
    Install `@sentry/node`, add the Express handlers and log Redis client errors.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 require('./lib/env');
 var new_relic = require('newrelic');
+var Sentry = require('@sentry/node');
 var Server = require('./lib/server');
+
+Sentry.init({
+  dsn: "https://4f4918f78b224ac19e4d32e99a165805@o274434.ingest.sentry.io/4504616446984192"
+});
 var port = parseInt(process.env.SUGAR_PORT) || 2999;
 var server = new Server();
 server.listen(port);
