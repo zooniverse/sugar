4992169c141056c14ef8e2acc6903e254c38f66c
Jim O'Donnell <jim@zooniverse.org>
2023-05-20 16:05:03 +0100

    Verify signed JWT to get logged-in user Replace Panoptes auth with `jwt.verify(token, key)`. Use the signed Panoptes JSON Web Token to get the logged-in user ID and display name (`data.id` and `data.dname` in the decoded token.)

--- test/server_socket_api.js
+++ test/server_socket_api.js
@@ -1,16 +1,22 @@
 const chai = require('chai');
+const jwt = require('jsonwebtoken');
 
 const expect = chai.expect;
 
 const SugarServer = require('./support/sugar_server');
-const PanoptesServer = require('./support/panoptes_server');
 
 describe('Server Socket API', function() {
   var client, sugar;
   sugar = null;
   client = null;
   beforeEach(function() {
-    PanoptesServer.mock();
+    const data = {
+      id: 1,
+      dname: 'user1'
+    };
+    jwt.verify = chai.spy(() => {
+      return { data }
+    });
     return SugarServer.create().then(function(server) {
       sugar = server;
       client = sugar.createClient('user_id=1&auth_token=valid_auth');
