3e1c2dfb299edbb944253b251ff58d92a40f47bf
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2024-04-14 16:38:49 +0100

    Remove chai-as-promised

--- test/client.mjs
+++ test/client.mjs
@@ -57,23 +57,23 @@ describe('SugarClient', function() {
   });
   describe('#primusUrl', function() {
     describe('when logged in', function() {
-      return it('should set the query string', function() {
+      it('should set the query string', function() {
         var baseUrl;
         baseUrl = {
           query: null
         };
         userClient.primusUrl(baseUrl);
-        return expect(baseUrl.query).to.eql('user_id=user&auth_token=auth');
+        expect(baseUrl.query).to.eql('user_id=user&auth_token=auth');
       });
     });
-    return describe('when not logged in', function() {
-      return it('should not set the query string', function() {
+    describe('when not logged in', function() {
+      it('should not set the query string', function() {
         var baseUrl;
         baseUrl = {
           query: null
         };
         sessionClient.primusUrl(baseUrl);
-        return expect(baseUrl.query).to.be(null);
+        expect(baseUrl.query).to.eql(null);
       });
     });
   });
